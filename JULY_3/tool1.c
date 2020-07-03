90c9e803d2f26fae3802dc38b7c06120e37b83ee
dragon-web <1582943265@qq.com>
2020-07-03 14:24:33 +0800

    JULY_3/

---
+++ JULY_3/tool1.c
@@ -0,0 +1,14 @@
+#ifndef TOOL1_H__
+#define TOOL1_H_
+#include<stdio.h>
+#include"tool1.h"
+
+void mytool1()
+{
+  printf("tool1 print\n");
+}
+
+
+
+
+#endif
---
+++ JULY_3/tool2.c
@@ -0,0 +1,14 @@
+#ifndef TOOL2_H__
+#define TOOL2_H_
+#include<stdio.h>
+#include"tool2.h"
+
+void mytool2()
+{
+  printf("tool2 print\n");
+}
+
+
+
+
+#endif
