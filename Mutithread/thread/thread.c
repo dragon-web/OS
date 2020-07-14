4d53a5ca4d0e89e053b415f37816fad785070c69
dragon-web <1582943265@qq.com>
2020-07-14 09:22:17 +0800

    Mutithread/

---
+++ Mutithread/thread/thread.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<pthread.h>
+#include<unistd.h>
+
+void* thread_run(void* arg)
+{
+  char* msg = (char*)arg;
+  while(1)
+  {
+    printf("new thread ...%s\n",msg);
+    sleep(2);
+  }
+
+}
+
+
+int main()
+{
+  pthread_t tid;
+  pthread_create(&tid,NULL,thread_run,(void*)"thread one");
+  while(1)
+  {
+    printf("main thread ...\n");
+    sleep(3);
+  }
+
+  return 0;
+}
